53f47ae37b5c47df6881b18a0feaf740042a8005
Zihad Malik <zihadm654@gmail.com>
2021-09-02 17:07:02 +0600

    alt tag added

--- src/components/SCard.jsx
+++ src/components/SCard.jsx
@@ -4,7 +4,7 @@ const SCard = ({ img, location, distance }) => {
   return (
     <div className="card">
       <div className="card__left">
-        <Image src={img} layout="fill" objectFit="fit" />
+        <Image src={img} layout="fill" objectFit="fit" alt="img" />
       </div>
       <div className="card__right">
         <h5>{location}</h5>
