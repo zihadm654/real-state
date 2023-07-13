ec858267587e56340ea57a106de5bc8260be6cb3
Abdul Malek <zihadm654@gmail.com>
2023-07-13 08:35:02 +0600

    new upgrade

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['links.papareact.com'],
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
